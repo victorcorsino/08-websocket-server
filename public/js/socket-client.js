f7644137e9ee4588ec2607ddd66606d7aa615600
victorcorsino <vcorsinoa@gmail.com>
2022-04-13 12:50:48 -0400

    Primer commit

---
+++ public/js/socket-client.js
@@ -0,0 +1,44 @@
+
+//Referencias del HTML
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+const txtMensaje = document.querySelector('#txtMensaje');
+const btnEnviar = document.querySelector('#btnEnviar');
+
+
+const socket = io();
+
+
+socket.on('connect', () => {
+    // console.log('Conectado');
+
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = '';
+});
+
+socket.on('disconnect', () => {
+    // console.log('Desconectado del servidor');
+
+    lblOffline.style.display = '';
+    lblOnline.style.display = 'none';
+});
+
+socket.on('enviar-mensaje', (payload) => {
+    console.log(payload);
+})
+
+
+btnEnviar.addEventListener( 'click', () => {
+
+    const mensaje = txtMensaje.value;
+    const payload = {
+        mensaje,
+        id: '123ABC',
+        fecha: new Date().getTime()
+    }
+
+    socket.emit('enviar-mensaje', payload, (id) => {
+        console.log('Desde el server', id);
+
+    });
+})
---
+++ sockets/controller.js
@@ -0,0 +1,27 @@
+
+const socketController = (socket) => {
+    console.log('Cliente conectado', socket.id);
+    
+    socket.on('disconnect', () => {
+        console.log('CLiente desconectado', socket.id);
+    })
+
+    socket.on('enviar-mensaje', (payload, callback) => {
+
+        const id = 123456;
+        callback(id);
+
+        socket.broadcast.emit('enviar-mensaje', payload);
+    })
+}
+
+
+
+module.exports = {
+    socketController
+}
+
+
+
+
+
